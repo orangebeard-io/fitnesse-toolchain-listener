f9297c692cc0429f06764b7481f409425acf48b6
Maarten-Jan <maarten-jan@orangebeard.io>
2021-11-12 12:46:11 +0100

    [team-soju#3833] fix picking up maven property

--- src/main/java/io/orangebeard/listener/ChangedComponentsHelper.java
+++ src/main/java/io/orangebeard/listener/ChangedComponentsHelper.java
@@ -4,16 +4,24 @@ import io.orangebeard.client.entity.ChangedComponent;
 
 import java.util.Arrays;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.util.stream.Collectors.toSet;
 
 public class ChangedComponentsHelper {
+    private static final Logger logger = LoggerFactory.getLogger(OrangebeardTestSystemListener.class);
+
     private ChangedComponentsHelper() {
         // static class
     }
 
     public static Set<ChangedComponent> getChangedComponents() {
         String changedComponents = System.getenv("orangebeard.changedComponents");
+        if (changedComponents == null || changedComponents.isEmpty()) {
+            changedComponents = System.getProperty("orangebeard.changedComponents");
+        }
+        logger.info("Changed components: [" + changedComponents + "]");
         if (changedComponents == null) {
             return null;
         }
