c971f337fae700747f851a1d534f98f232f78760
Jyothi Nalla <jyothi@orangebeard.io>
2022-06-12 22:49:39 +0200

    [team-soju#4072]changes with v3 api

---
+++ src/main/java/io/orangebeard/listener/v3client/entities/StartTestRun.java
@@ -0,0 +1,63 @@
+package io.orangebeard.listener.v3client.entities;
+
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@EqualsAndHashCode
+public class StartTestRun {
+
+    @NotNull
+    @Size(min = 1, max = 128)
+    private final String testSetName;
+
+    @Size(min = 1)
+    private final String description;
+
+    @NotNull
+    private final ZonedDateTime startTime;
+
+    @Valid
+    private final Set<Attribute> attributes;
+
+    @Valid
+    private final List<ChangedComponent> changedComponents;
+
+    @JsonIgnore
+    public Set<io.orangebeard.datatypes.ChangedComponent> getChangedComponentDatatypes() {
+        if (changedComponents == null || changedComponents.isEmpty()) {
+            return null;
+        }
+        return changedComponents.stream()
+                .filter(Objects::nonNull)
+                .filter(it -> it.getComponentName() != null && !it.getComponentName().equals(""))
+                .map(ChangedComponent::toDataType)
+                .collect(Collectors.toSet());
+    }
+
+    @JsonIgnore
+    public Set<io.orangebeard.datatypes.Attribute> getAttributesDataTypes() {
+        if (attributes == null || attributes.isEmpty()) {
+            return null;
+        }
+        return attributes
+                .stream()
+                .filter(Objects::nonNull)
+                .map(it -> new io.orangebeard.datatypes.Attribute(it.getKey(), it.getValue()))
+                .collect(Collectors.toSet());
+    }
+}
---
+++ src/main/java/io/orangebeard/listener/v3client/entities/TestStatus.java
@@ -0,0 +1,9 @@
+package io.orangebeard.listener.v3client.entities;
+
+public enum TestStatus {
+    PASSED,
+    FAILED,
+    SKIPPED,
+    STOPPED,
+    TIMED_OUT
+}
